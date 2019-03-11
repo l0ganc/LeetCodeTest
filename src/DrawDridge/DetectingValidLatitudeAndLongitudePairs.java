0e0e623b797390a3c1816b34371ab956c09538b8
l0ganc <jingyige@usc.edu>
2019-03-11 16:05:34 -0700

    经纬度检验

---
+++ src/DrawDridge/DetectingValidLatitudeAndLongitudePairs.java
@@ -0,0 +1,10 @@
+package DrawDridge;
+
+/**
+ * Given a list of strings that may represent valid latitude/longitute pairs, test for validity,
+ * either valid or invalid representing your results for each test
+ *
+ * shoule use regix to rule out
+ */
+public class DetectingValidLatitudeAndLongitudePairs {
+}
