e965896f70a1d988ba51b73fc46019e706bbe9f8
l0ganc <jingyige@usc.edu>
2019-05-26 22:37:47 -0700

    LC973

---
+++ src/Amazon社招OA/LC973KClosestPointsToOrigin.java
@@ -0,0 +1,23 @@
+package Amazon社招OA;
+
+import java.util.PriorityQueue;
+
+public class LC973KClosestPointsToOrigin {
+
+    // time = O(NlogK)
+
+    public static int[][] kClosest(int[][] points, int K) {
+        PriorityQueue<int[]> pq = new PriorityQueue<int[]>((p1, p2) -> p2[0] * p2[0] + p2[1] * p2[1] - p1[0] * p1[0] - p1[1] * p1[1]);
+        for (int[] p : points) {
+            pq.offer(p);
+            if (pq.size() > K) {
+                pq.poll();
+            }
+        }
+        int[][] res = new int[K][2];
+        while (K > 0) {
+            res[--K] = pq.poll();
+        }
+        return res;
+    }
+}
