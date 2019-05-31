9a01f97228991bff26ea990086e3ee5f6e014bb0
l0ganc <jingyige@usc.edu>
2019-05-31 16:56:27 -0700

    LC

--- src/Amazon社招OA/LC973KClosestPointsToOrigin.java
+++ src/Amazon社招OA/LC973KClosestPointsToOrigin.java
@@ -1,6 +1,6 @@
 package Amazon社招OA;
 
-import java.util.PriorityQueue;
+import java.util.*;
 
 public class LC973KClosestPointsToOrigin {
 
@@ -19,5 +19,37 @@ public class LC973KClosestPointsToOrigin {
             res[--K] = pq.poll();
         }
         return res;
+    }
+
+    public static void main(String[] args) {
+        String s = "The rain in Spain falls mainly in the plain.";
+        System.out.println((s.split(" ", 2))[1]);
+
+        List<Integer> re = new ArrayList<>();
+
+        re.add(1);
+        re.add(2);
+        re.add(6);
+        re.add(5);
+        re.add(8);
+        re.add(8);
+        re.add(4);
+
+        Collections.sort(re, new Comparator<Integer>() {
+
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                //下面这么写，结果是降序
+                if(o1 < o2){
+                    return -1;
+                }else if(o1 > o2){
+                    return 1;
+                }
+                return 0;
+            }
+
+        });
+
+        System.out.println(re);
     }
 }
